84d2883e8c2c4fbc2fb464fca86a5b43d79c045c
rlharsh <ronald.harsh@protonmail.com>
2023-10-18 20:07:49 -0500

    Update JS

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,87 +1,94 @@
-import { itemDatabase } from './firebase.js';
+import { itemDatabase } from "./firebase.js";
 
 const categoryListing = [
-    {
-        name: "Burgers",
-        icon: "Burger.svg",
-        color: "#FFEF92"
-    },
-    {
-        name: "Fries & More",
-        icon: "Fries.svg",
-        color: "#F5CAC3"
-    },
-    {
-        name: "Chicken",
-        icon: "Burger.svg",
-        color: "#F7EDE2"
-    },
-    {
-        name: "Desserts",
-        icon: "Desserts.svg",
-        color: "#A9D7DA"
-    },
-    {
-        name: "Drinks",
-        icon: "Drinks.svg",
-        color: "#B6D7CF"
-    },
-    {
-        name: "Coffee",
-        icon: "Coffee.svg",
-        color: "#F5F5F5"
-    }
+  {
+    name: "Burgers",
+    icon: "Burger.svg",
+    color: "#FFEF92",
+  },
+  {
+    name: "Fries & More",
+    icon: "Fries.svg",
+    color: "#F5CAC3",
+  },
+  {
+    name: "Chicken",
+    icon: "Burger.svg",
+    color: "#F7EDE2",
+  },
+  {
+    name: "Desserts",
+    icon: "Desserts.svg",
+    color: "#A9D7DA",
+  },
+  {
+    name: "Drinks",
+    icon: "Drinks.svg",
+    color: "#B6D7CF",
+  },
+  {
+    name: "Coffee",
+    icon: "Coffee.svg",
+    color: "#F5F5F5",
+  },
 ];
 
 const loadCategories = () => {
-    let itemFragment = document.createDocumentFragment();
+  let itemFragment = document.createDocumentFragment();
 
-    categoryListing.forEach(item => {
-        let itemCard = document.createElement('div');
-        itemCard.classList.add('category-item');
-        itemCard.style.backgroundColor = item.color;
+  categoryListing.forEach((item) => {
+    let itemCard = document.createElement("div");
+    itemCard.classList.add("category-item");
+    itemCard.style.backgroundColor = item.color;
 
-        let itemName = document.createElement('p');
-        itemName.innerText = item.name;
-        itemCard.appendChild(itemName);
+    let itemName = document.createElement("p");
+    itemName.innerText = item.name;
+    itemCard.appendChild(itemName);
 
-        let itemImage = document.createElement('img');
-        itemImage.src = `./assets/images/food/${item.icon}`;
-        itemImage.classList.add('category-item__image');
-        itemCard.appendChild(itemImage);
+    let itemImage = document.createElement("img");
+    itemImage.src = `./assets/images/food/${item.icon}`;
+    itemImage.classList.add("category-item__image");
+    itemCard.appendChild(itemImage);
 
-        itemCard.addEventListener('click', () => {
-            processClickCategory(item.name);
-        });
-
-        itemFragment.appendChild(itemCard);
+    itemCard.addEventListener("click", () => {
+      processClickCategory(item.name);
     });
 
-    document.getElementById('categories').appendChild(itemFragment);
+    itemFragment.appendChild(itemCard);
+  });
+
+  document.getElementById("categories").appendChild(itemFragment);
 };
 
 const processClickCategory = (category) => {
-    let itemsInCategory = [];
-    itemDatabase.forEach(item => {
-        if (item.category === category.toLowerCase()) {
-            itemsInCategory.push(item);
-        }
-    });
-    showCategory(itemsInCategory);
+  let itemsInCategory = [];
+  itemDatabase.forEach((item) => {
+    if (item.category === category.toLowerCase()) {
+      itemsInCategory.push(item);
+    }
+  });
+  showCategory(itemsInCategory);
+};
+
+const hideMain = () => {
+  const appDiv = document.getElementById("app");
+  appDiv.classList.add("no-scroll");
+  console.log("NO SCROLL");
 };
 
 const showCategory = (items) => {
-    const categoryContainer = document.getElementById('modal-category');
+  const categoryContainer = document.getElementById("modal-category");
+  if (categoryContainer.classList.contains("hidden")) {
+    categoryContainer.classList.remove("hidden");
+  }
 
-    if (categoryContainer.classList.contains('hidden')) {
-        categoryContainer.classList.remove('hidden');
-    }
+  hideMain();
 
-    let itemFragment = document.createDocumentFragment();
-    const categoryListingContainer = document.getElementById('category-lising-container');
-    items.forEach(item => {
-
-    });
+  let itemFragment = document.createDocumentFragment();
+  const categoryListingContainer = document.getElementById(
+    "category-lising-container"
+  );
+  items.forEach((item) => {});
 };
 
 loadCategories();
